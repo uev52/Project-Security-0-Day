791fe0c2df23e1625c1847fa71b7e9119cca4eb5
Twen Elf <twenelf@gmail.com>
2020-12-27 18:35:15 +0100

    Controlle
    Gedaan:
        - Controlle toegevoegd zodat als de gegevens verkeerd zijn ze veranderd kunnen worden

--- backupRecovery.py
+++ backupRecovery.py
@@ -10,17 +10,27 @@ def locationcheck(Location, string): # verantwoordelijk voor het controleren of 
             Location = input(string)
     return Location
 
-backupLocation = input("Wat is de locatie van de back-up: ") # vraagt de locatie van de backup
-backupLocation=locationcheck(backupLocation, "Wat is de locatie van de back-up: ") # controleert of de opgegeven locatie correct is
+def ask():
+    backupLocation = input("Wat is de locatie van de back-up: ") # vraagt de locatie van de backup
+    backupLocation=locationcheck(backupLocation, "Wat is de locatie van de back-up: ") # controleert of de opgegeven locatie correct is
 
-toLocation = input("Wat is de locatie waar het naar toe moet: ") # vraagt waar de backup naartoe moet
-toLocation = locationcheck(toLocation,"Wat is de locatie waar het naar toe moet:")# controleert of de opgegeven locatie correct is
+    toLocation = input("Wat is de locatie waar het naar toe moet: ") # vraagt waar de backup naartoe moet
+    toLocation = locationcheck(toLocation,"Wat is de locatie waar het naar toe moet:")# controleert of de opgegeven locatie correct is
 
-if (os.path.isfile(backupLocation)) == True: # WIP bedoelt om losse bestanden te kunnen doen
-    pass
+    if (os.path.isfile(backupLocation)) == True: # WIP bedoelt om losse bestanden te kunnen doen
+        pass
 
-print("\nBackup: {}\nTo: {}".format(backupLocation,toLocation)) # Geeft gegevens die zijn ingevuld terug voor controlle
+    print("\nBackup: {}\nTo: {}".format(backupLocation,toLocation)) # Geeft gegevens die zijn ingevuld terug voor controlle
 
+    check = input("\nDit zijn de gegevens klopt dit? (y,n,c) ") # vraagt of de gegevens goed zijn
+    if check.lower() == "n":  # als dat niet het geval is vraag hij nog eens
+        backupLocation, toLocation = ask()
+    elif check.lower() == "c":
+        quit()
+    
+    return backupLocation, toLocation
+
+backupLocation, toLocation = ask()
 
 DirectoryFiles = os.listdir(backupLocation) # verkrijgt de naam van alle bestanden in de backup directory
 os.chdir(backupLocation) # zodat het controleren op dir makelijker is
@@ -31,4 +41,4 @@ for file in DirectoryFiles:
                 shutil.copy(file, toLocation) # hier copieerd hij het
         else:
                 pass
-            
+print("Done")
