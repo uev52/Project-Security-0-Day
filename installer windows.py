c7dd975c888b1788777b588d56b2966d4c9fce5c
Twen Elf <twenelf@gmail.com>
2020-12-31 03:21:09 +0100

    automatisatie
    Voor het automatiseren van de task update en het installeren van alle python packages zodat er geen problemen komen

---
+++ installer windows.py
@@ -0,0 +1,10 @@
+import pip
+pip.main(['install', 'paramiko'])
+pip.main(['install', 'os'])
+pip.main(['install', 'shutil'])
+pip.main(['install', 'platform'])
+pip.main(['install', 'tkinter'])
+pip.main(['install', 'pillow'])
+pip.main(['install', 'PyPDF2'])
+pip.main(['install', 'python3-tk'])
+
---
+++ taskmake.ps1
@@ -0,0 +1,25 @@
+# Create a new task action
+$filename = "{Filepath}"
+
+$taskAction = New-ScheduledTaskAction `
+    -Execute 'powershell.exe' `
+    -Argument '-File $filename'
+$taskAction
+
+$taskTrigger = New-ScheduledTaskTrigger -Daily -At 9AM
+$tasktrigger
+
+# Register the new PowerShell scheduled task
+
+# The name of your scheduled task.
+$taskName = "Update server"
+
+# Describe the scheduled task.
+$description = "Update de server"
+
+# Register the scheduled task
+Register-ScheduledTask `
+    -TaskName $taskName `
+    -Action $taskAction `
+    -Trigger $taskTrigger `
+    -Description $description
