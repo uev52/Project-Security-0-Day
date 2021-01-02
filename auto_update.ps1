9efb29f05000036ce1ba66d6ad4d47f119868fb7
timkunst22 <62659528+timkunst22@users.noreply.github.com>
2021-01-02 15:41:22 +0100

    Update auto_update.ps1
    aangepast na testen op windows server 2019 en werkt

--- auto_update.ps1
+++ auto_update.ps1
@@ -1,13 +1,13 @@
 # Commend de volgende commando's uit als de modules niet geïnstaleerd zijn
 # Install-Module -Name PSWindowsUpdate
-# Save-Module -Name PSWindowsUpdate -Path
+# Save-Module -Name PSWindowsUpdate -Path C:\
 
 # Voor het ophalen van de updates
 Get-WindowsUpdate
 
 # Voor het downlaoden van updates en niet rebooten
-Get-WUInstall -AcceptAll –IgnoreReboot
+Get-WUInstall -AcceptAll –IgnoreReboot -Install
 
 # volgend commando uit commenten als reboot gewild is
-# Get-WUInstall -AcceptAll –AutoReboot
+# Get-WUInstall -AcceptAll –AutoReboot -Install
 
